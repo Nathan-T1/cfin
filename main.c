f7683cd7d40b3f184dc9067852336661dfbd1b56
Nathan-T1 <49128392+Nathan-T1@users.noreply.github.com>
2021-10-18 11:16:46 -0500

    Add files via upload
    Updated csv reader

--- main.c
+++ main.c
@@ -1,34 +1,91 @@
-
-#define PY_SSIZE_T_CLEAN
-#include "Python.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <stdbool.h>
 
-static PyObject* system_call(PyObject *self, PyObject *args){
-    const char *command;
-    int sts;
-
-    if(!PyArg_ParseTuple(args, "s", &command)){
-      return NULL;
-    };
-    sts = system(command);
-    if(sts < 0){
-       PyErr_SetString(PyExc_ValueError, "System Command Failed");
-       return NULL;
+#define BUFFER_SIZE 200
+#define HEADER_SIZE 30
+struct Point_ {
+    char* t;
+    float open;
+    float high;
+    float low;
+    float close;
+    unsigned long volume;
+};
+static void free_char_array(char** arr, int size){
+    for(size_t i = 0; i < size; i++){
+        free(arr[i]);
+    }
+    free(arr);
+}
+void removeNewLineChar(char *ptr){
+    while((ptr != NULL) && (*ptr != '\n')){
+        ++ptr;
+    }
+    *ptr = '\0';
+}
+static void handle_row(char* buffer, const char* const delim, struct timeval* tv, float* step, int cols){
+    char* token;
+    
+    token = strtok(buffer, delim);
+    while(token){
+        printf("%s \n", token);
+        token = strtok(NULL,",");
+    }
+}
+int read_csv(char* file, const char* const delim){
+    FILE* fp;
+    char* buffer;
+    int columns = 0; 
+    int rows = 0;
+    
+    char **headers = (char**) calloc(1, sizeof(char*));
+    float **arr = (float**) calloc(1, sizeof(float*));
+    struct timeval *tr = (struct timeval*)malloc(1*sizeof(struct timeval));
+    
+    if(headers == NULL || arr == NULL || tr == NULL){
+        fprintf(stderr, "Error: Error allocating memory");
+    }
+    if((fp = fopen(file, "r")) == NULL){
+        fprintf(stderr, "Error: File does not exist");
+        return 0;
+    }
+    if(!(buffer = (char*)calloc(BUFFER_SIZE, sizeof(char)))){
+        fprintf(stderr, "Error: Error allocating buffer");
+        return 0;
+    }
+    fgets(buffer, BUFFER_SIZE, fp);
+    removeNewLineChar(buffer);
+    
+    char *token = strtok(buffer, delim);
+    while (token) {
+        headers = (char**) realloc(headers, (columns+1)*sizeof(char*));
+        size_t length = strlen(token);
+        headers[columns] = (char*)calloc(length, sizeof(char));
+        strcpy(headers[columns],token);
+        columns++;
+        token = strtok(NULL, ",");
+    }
+    while(feof(fp) != true){
+        arr = (float**) realloc(arr, (rows + 1)*sizeof(float*));
+        tr = (struct timeval*) realloc(tr, (rows + 1)*sizeof(struct timeval));
+        arr[rows] = (float*)calloc(columns, sizeof(float));
+        
+        fgets(buffer, BUFFER_SIZE, fp);
+        handle_row(buffer, delim, &tr[rows], arr[rows], columns);
+        rows++;
+        
     }
 
-    return PyLong_FromLong(sts);
-};
-static PyMethodDef system_call_methods[] = {
-    {"system_call", system_call, METH_VARARGS, "Execute Shell Command"}
-};
-static struct PyModuleDef systemModule = {
-    PyModuleDef_HEAD_INIT,
-    "cfin",   /* name of module */
-    "cfin Module", /* module documentation, may be NULL */
-    -1,       /* size of per-interpreter state of the module,
-                 or -1 if the module keeps state in global variables. */
-    system_call_methods
-};
-PyMODINIT_FUNC PyInit_cfin(void){
-    return PyModule_Create(&systemModule);
-};
+    free_char_array(headers, columns);
+    return 1;
+}
+
+int main() {
+	char* path = "test.csv";
+    const char* const s = ",";
+    read_csv(path, s);
+	return 0;
+}
